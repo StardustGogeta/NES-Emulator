0710e3fc47c30773dc904d3097f5653e1c0ef78f
StardustGogeta <liam.gerst@gmail.com>
2017-10-04 22:12:40 -0400

    Adding some source files
    Now that I have actually learned how to make an SDL2 project, I might finally be able to attempt this.

---
+++ src/main.cpp
@@ -0,0 +1,64 @@
+#include "main.h"
+using namespace std;
+
+void loadRom(string path) {
+    ifstream ROM;
+    ROM.open(path, ifstream::ate | ifstream::binary);
+    int ROMsize = ROM.tellg(); // Find file length
+    cout << "The file size is " << ROMsize << " bytes.\n";
+    ROM.seekg(0); // Reset to start of file
+    char header[16];
+    for (int x=0; x<16; x++) {
+        ROM >> hex >> header[x];
+        // cout << setfill('0') << setw(2) << hex << (int)header[x] << " Byte" << endl;
+    }
+
+    if (!(header[0] == 0x4e && header[1] == 0x45 && header[2] == 0x53 && header[3] == 0x1a)) {
+        cout << "The NES file header is invalid.\n";
+    } // Check that the first 4 characters are "NES\n"
+
+    PRG_ROM_size = header[4], CHR_ROM_size = header[5],
+    flags6 = header[6], flags7 = header[7], PRG_RAM_size = header[8],
+    flags9 = header[9], flags10 = header[10]; // Identify all header components
+
+    // Decode flags 6 and 7
+    mirroring = flags6 & 0b00000001 ? "vertical" : "horizontal";
+    persistentMemory    = flags6 & 0b00000010;
+    trainer             = flags6 & 0b00000100;
+    fourScreenVRAM      = flags6 & 0b00001000;
+    mapper              = flags6 & 0b11110000 + ((flags7 & 0b11110000) >> 4);
+    NES2                =(flags7 & 0b00001100) == 0b1000; // NES 2.0 not fully supported
+    playchoice10        = flags7 & 0b00000010;
+    VS_unisystem        = flags7 & 0b00000001;
+    // cout << hex << (int)PRG_ROM_size << " PRGROM\n" << (int)PRG_RAM_size << " PRGRAM\n" << (int)mapper << " Mapper\n";
+
+    /* Header format
+    0-3: Constant $4E $45 $53 $1A ("NES" followed by MS-DOS end-of-file)
+    4: Size of PRG ROM in 16 KB units
+    5: Size of CHR ROM in 8 KB units (Value 0 means the board uses CHR RAM)
+    6: Flags 6
+    7: Flags 7
+    8: Size of PRG RAM in 8 KB units (Value 0 infers 8 KB for compatibility; see PRG RAM circuit)
+    9: Flags 9
+    10: Flags 10 (unofficial)
+    11-15: Zero filled
+    */
+
+    ROM.close();
+}
+
+void initStorage() {}
+
+int main(int argc, char* argv[]) {
+    cout << "Hello world!\n";
+    string path;
+    if (argc > 1) {
+        path = argv[1];
+        cout << "File argument: " << path << endl;
+    }
+    else {
+        path = "mario.nes";
+        cout << "Defaulting to " << path << endl;
+    }
+    loadRom(path);
+}
