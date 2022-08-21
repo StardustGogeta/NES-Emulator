16d824af8fd91333ca683d6833b0bb601520b42c
StardustGogeta <liam.gerst@gmail.com>
2022-08-21 11:23:39 -0400

    Separating memory management into different file

--- src/rom.h
+++ src/rom.h
@@ -1,6 +1,10 @@
 #pragma once
+#include <cstring>
+#include <fstream>
 
-extern char header[16], memory[0x10000], PRG_ROM_size, CHR_ROM_size,
+// Data
+
+extern uint8_t header[16], memory[0x10000], PRG_ROM_size, CHR_ROM_size,
     flags6, flags7, PRG_RAM_size, flags9, flags10, mapper;
 
 extern std::string mirroring;
@@ -8,5 +12,7 @@ extern std::string mirroring;
 extern bool persistentMemory, trainer, fourScreenVRAM, NES2, playchoice10, VS_unisystem;
 
 extern std::ifstream ROM;
+
+// Functions
 
 void loadRom(std::string);
