d56851f2af4ff78bb38595b17218cbe344e4230a
Zach Breit <zbreit18@gmail.com>
2025-11-29 15:47:32 -0500

    Swap to fprintf from std::print
    This improves performance

--- src/cpu/logger.cpp
+++ src/cpu/logger.cpp
@@ -1,11 +1,11 @@
 #include "cpu.h"
-#include <ostream>
+#include <cstdio>
 #include <print>
 
 CPU::Logger::Logger(CPU& cpu) : logging(false), reversePPU(false), cpu(cpu) { }
 
 void CPU::Logger::start(std::string path, bool newReversePPU /* = false */) {
-    logFile.open(path, std::ios::out);
+    logFile = fopen(path.c_str(), "w");
     logging = true;
     // Nintendulator and nestest seem to use reversed PPU cycle notations
     // Setting this to true will make it Nintendulator-compatible
@@ -14,29 +14,38 @@ void CPU::Logger::start(std::string path, bool newReversePPU /* = false */) {
 }
 
 void CPU::Logger::stop() {
-    logFile.close();
+    fclose(logFile);
+    logFile = nullptr;
     std::println("Stopped CPU logging.");
 }
 
+CPU::Logger::~Logger() {
+    if (logFile) {
+        fclose(logFile);
+        logFile = nullptr;
+    }
+}
+
+
 void CPU::Logger::logOpcode(uint8_t opcode, addressingMode mode, instruction inst) {
-    std::print(logFile, "{:04X}  {:02X}", cpu.pc - 1, opcode);
+    fprintf(logFile, "%04X  %02X", cpu.pc - 1, opcode);
         
     // Write opcode arguments to log
     int count = addressingModeReadCount[mode];
     if (count) {
-        std::print(logFile, " {:02X}", cpu.memory->read(cpu.pc));
+        fprintf(logFile, " %02X", cpu.memory->read(cpu.pc));
         if (count > 1) {
-            std::print(logFile, " {:02X}", cpu.memory->read(cpu.pc + 1));
+            fprintf(logFile, " %02X", cpu.memory->read(cpu.pc + 1));
         } else {
-            std::print(logFile, "   ");
+            fprintf(logFile, "   ");
         }
     } else {
-        std::print(logFile, "      ");
+        fprintf(logFile, "      ");
     }
 
     // Write opcode name to log
     std::string opcodeName = opcodeNames[inst];
-    std::print(logFile, "{}", (isLegalOpcode(opcode) ? "  " : " *") + opcodeName + " ");
+    fprintf(logFile, "%s", ((isLegalOpcode(opcode) ? "  " : " *") + opcodeName + " ").c_str());
 }
 
 void CPU::Logger::logArgsAndRegisters(addressingMode mode, instruction inst, addr_t addr, uint8_t argument) {
@@ -50,56 +59,56 @@ void CPU::Logger::logArgsAndRegisters(addressingMode mode, instruction inst, add
     }
     switch (mode) {
         case IMM:
-            std::print(logFile, "#${:02X}                        ", argument);
+            fprintf(logFile, "#$%02X                        ", argument);
             break;
         case ZPG:
-            std::print(logFile, "${:02X} = {:02X}                    ", addr, argument);
+            fprintf(logFile, "$%02X = %02X                    ", addr, argument);
             break;
         case ZPX:
-            std::print(logFile, "${:02X},X @ {:02X} = {:02X}             ", cpu.cache, addr, argument);
+            fprintf(logFile, "$%02X,X @ %02X = %02X             ", cpu.cache, addr, argument);
             break;
         case ZPY:
-            std::print(logFile, "${:02X},Y @ {:02X} = {:02X}             ", cpu.cache, addr, argument);
+            fprintf(logFile, "$%02X,Y @ %02X = %02X             ", cpu.cache, addr, argument);
             break;
         case IZX:
-            std::print(logFile, "(${:02X},X) @ {:02X} = {:04X} = {:02X}    ", cpu.precache, cpu.cache, addr, argument);
+            fprintf(logFile, "($%02X,X) @ %02X = %04X = %02X    ", cpu.precache, cpu.cache, addr, argument);
             break;
         case IZY:
-            std::print(logFile, "(${:02X}),Y = {:04X} @ {:04X} = {:02X}  ", cpu.precache, cpu.cache, addr, argument);
+            fprintf(logFile, "($%02X),Y = %04X @ %04X = %02X  ", cpu.precache, cpu.cache, addr, argument);
             break;
         case ABS:
-            std::print(logFile, "${:04X}", addr);
+            fprintf(logFile, "$%04X", addr);
             if (inst == JMP || inst == JSR) {
-                std::print(logFile, "                       ");
+                fprintf(logFile, "                       ");
             } else {
-                std::print(logFile, " = {:02X}                  ", argument);
+                fprintf(logFile, " = %02X                  ", argument);
             }
             break;
         case ABX:
-            std::print(logFile, "${:04X},X @ {:04X} = {:02X}         ", cpu.cache, addr, argument);
+            fprintf(logFile, "$%04X,X @ %04X = %02X         ", cpu.cache, addr, argument);
             break;
         case ABY:
-            std::print(logFile, "${:04X},Y @ {:04X} = {:02X}         ", cpu.cache, addr, argument);
+            fprintf(logFile, "$%04X,Y @ %04X = %02X         ", cpu.cache, addr, argument);
             break;
         case IND:
-            std::print(logFile, "(${:04X}) = {:04X}              ", cpu.cache, addr);
+            fprintf(logFile, "($%04X) = %04X              ", cpu.cache, addr);
             break;
         case REL:
-            std::print(logFile, "${:04X}                       ", addr);
+            fprintf(logFile, "$%04X                       ", addr);
             break;
         case NUL:
             if (inst == ASL || inst == LSR || inst == ROL || inst == ROR) {
-                std::print(logFile, "A                           ");
+                fprintf(logFile, "A                           ");
             } else {
-                std::print(logFile, "                            ");
+                fprintf(logFile, "                            ");
             }
             break;
         default:
-            std::print(logFile, "ERROR                           ");
+            fprintf(logFile, "ERROR                           ");
             break;
     }
 
-    std::print(logFile, "A:{:02X} X:{:02X} Y:{:02X} P:{:02X} SP:{:02X}", cpu.a, cpu.x, cpu.y, cpu.processorStatus(), cpu.sp);
+    fprintf(logFile, "A:%02X X:%02X Y:%02X P:%02X SP:%02X", cpu.a, cpu.x, cpu.y, cpu.processorStatus(), cpu.sp);
 }
 
 /*
@@ -109,14 +118,15 @@ void CPU::Logger::logPPUInfo(int scanline, int cyclesOnLine) {
     int first = reversePPU ? cyclesOnLine : scanline;
     int second = reversePPU ? scanline : cyclesOnLine;
 
-    std::print(logFile, " PPU:{:3},{:3}", first, second);
+    fprintf(logFile, " PPU:%3d,%3d", first, second);
 }
 
 /*
     Writes an arbitrary string to the current log file.
 */
 void CPU::Logger::logStr(std::string_view str) {
-    std::print(logFile, "{}", str);
+    // TODO: .data() is not always a c string
+    fprintf(logFile, "%s", str.data());
 }
 
 void CPU::Logger::logCycles(int cyclesExecuted) {
