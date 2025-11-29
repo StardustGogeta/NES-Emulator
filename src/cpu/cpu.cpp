c2f1dd56bb86f5c327a404250478c2e4845ee339
Zach Breit <zbreit18@gmail.com>
2025-11-29 14:54:46 -0500

    Feature/single threaded (#21)
    * Add Linux release with debug info preset
    
    * Remove locking primitives
    
    These primitives were not used in the current software build and slowed down the program.
    
    * Swap to logCompare script for nestest
    
    logCompare.py provides better error messages on failure
    
    * Remove WORKING_DIRECTORY
    CMake implicitly uses CMAKE_CURRENT_BINARY_DIR, which works for our purposes
    
    * Remove unnecessary heap allocation
    
    * Avoid creating string on each loop
    
    Instead, store the data for creating the string and write it out to the log file later on.
    
    * Swap to std::filesystem for ROM path
    This allows us to more easily normalize paths in the future, if desired.
    
    * Fix bug w uninitialized nmiState
    The nmiState flag was set to a garbage value on startup, leading to inconsistent behavior when running the executable.
    
    * Adjust comment
    
    * Fix log order
    
    * Remove comment
    
    * Address PR comments

--- src/cpu/cpu.cpp
+++ src/cpu/cpu.cpp
@@ -1,11 +1,11 @@
 #include "cpu.h"
 #include <bit>
+#include <cstdint>
 #include <stdexcept>
 #include <print>
 #include <cstring>
 
 CPU::CPU() : logger(*this) {
-    running = false;
     memory = nullptr;
     ppu = nullptr;
     reset();
@@ -18,9 +18,7 @@ void CPU::reset() {
     a = x = y = 0;
     memset(&p, 0, sizeof(p));
     p.b1 = p.i = 1;
-    cyclesRequested = 0;
     cyclesExecuted = 0;
-    maxCycles = 0;
 }
 
 void CPU::setPC(bool ignoreCycles /* = false */) {
@@ -31,7 +29,7 @@ void CPU::setPC(bool ignoreCycles /* = false */) {
         // Going to the initial PC location seems to take 14 cycles, per Nintendulator
         // We have a flag to disable this if it's unwanted
         if (!ignoreCycles) {
-            cyclesRequested = cyclesExecuted = 14;
+            cyclesExecuted = 14;
         }
     }
 }
@@ -144,8 +142,6 @@ void CPU::setProcessorStatus(uint8_t status) {
     ignoreCycles - true if the PPU should ignore the CPU cycles being run
 */
 void CPU::runOpcode(uint8_t opcode, bool ignoreCycles /* = false */) {
-    // std::thread ppuThread(&PPU::cycles, ppu, 3);
-
     // This slows things down a lot:
     // std::println("Trying to run opcode {:#04x} at position {:#06x}", opcode, pc - 1);
 
@@ -165,7 +161,9 @@ void CPU::runOpcode(uint8_t opcode, bool ignoreCycles /* = false */) {
     int cycleOffset = getCycleCountOffset(inst, addr, extraCycleCounts[opcode]);
     int cycleCount = getCycleCount(opcode, cycleOffset);
 
-    std::string ppuString = logger.logPPUstring(ppu->scanline, ppu->cyclesOnLine);
+    // Grab the old PPU values before cycling
+    int ppuScanLine = ppu->scanline;
+    int ppuCyclesOnLine = ppu->cyclesOnLine;
 
     if (!ignoreCycles) {
         // PPU does 3 cycles for every CPU cycle
@@ -190,131 +188,24 @@ void CPU::runOpcode(uint8_t opcode, bool ignoreCycles /* = false */) {
 
     runInstruction(mode, inst, addr, argument);
 
-    logger.logStr(ppuString);
-
-    logger.logCycles(cyclesExecuted);
+    if (logger.logging) {
+        logger.logPPUInfo(ppuScanLine, ppuCyclesOnLine);
+        logger.logCycles(cyclesExecuted);
+    }
 
     if (!ignoreCycles) {
-        // TODO: Get actual cycle count
         cyclesExecuted += cycleCount;
-
-        waitForCycles(cycleCount);
     }
 }
 
 /*
-    Causes the current thread to wait until the controlling thread calls CPU::cycle() n times.
-    Returns true if execution of the thread should continue and false if the thread is being killed.
+    Runs 1 CPU instruction (potentially taking multiple cycles)
 */
-bool CPU::waitForCycles(int n) {
-    for (int i = 0; i < n; i++) {
-        if (!waitForCycle()) {
-            return false;
-        }
+void CPU::runNextInstruction() {
+    if (ppu->checkNmiFlag()) {
+        handleNonMaskableInterrupt();
     }
-    return true;
-}
-
-bool CPU::checkRunning() {
-    return running;
-}
-
-/*
-    Causes the current thread to wait until the controlling thread calls CPU::cycle().
-    Returns true if execution of the thread should continue and false if the thread is being killed.
-*/
-bool CPU::waitForCycle() {
-    std::unique_lock lck(cycleStatusMutex);
-
-    bool caughtUp = cyclesRequested <= cyclesExecuted;
-    notDone = cyclesExecuted < maxCycles || !maxCycles;
-
-    // This slows things down a lot:
-    // std::println("Executed {} cycles of {} requested and max {}", cyclesExecuted.load(), cyclesRequested, maxCycles);
-
-    if (running && caughtUp && notDone) {
-        // We are caught up, so we need to wait until the next cycle is requested.
-        cycleStatusCV.wait(lck, [this] {
-            // Check if we have more cycles in the queue to execute.
-            bool moreCycles = cyclesRequested > cyclesExecuted;
-            // Check if we have already hit the maximum number of cycles.
-            notDone = cyclesExecuted < maxCycles || !maxCycles;
-            // Check if we have been manually killed.
-            bool killed = !running;
-            // If any are true, we need to stop waiting and move on.
-            return killed || moreCycles || !notDone;
-        });
-    }
-
-    return running && notDone;
-}
-
-/*
-    Continually runs opcodes until stopped by CPU::stop().
-*/
-void CPU::start() {
-    std::unique_lock lck(cycleStatusMutex);
-    notDone = true;
-    running = true;
-    lck.unlock();
-
-    while (running && notDone) {
-        if (ppu->checkNmiFlag()) {
-            handleNonMaskableInterrupt();
-        }
-        else {
-            runOpcode(read());
-        }
-    }
-}
-
-/*
-    Stops the CPU from running by ending after the currently queued opcodes.
-    Argument t is the thread running CPU::start() that needs to end.
-*/
-void CPU::stop(std::thread& t) {
-    std::unique_lock lck(cycleStatusMutex);
-    maxCycles = cyclesRequested;
-    lck.unlock();
-
-    // Stop waiting for the next cycle, if that is the current thread state.
-    cycleStatusCV.notify_all();
-
-    // Join the thread afterwards.
-    if (t.joinable()) {
-        t.join();
-    }
-}
-
-/*
-    Stops the CPU from running by ending after the current opcode.
-    Causes the condition in CPU::start() to be false.
-    Argument t is the thread running CPU::start() that needs to end.
-    This is more forceful than CPU::stop(), which allows queued opcodes to finish.
-*/
-void CPU::kill(std::thread& t) {
-    std::unique_lock lck(cycleStatusMutex);
-    running = false;
-    lck.unlock();
-
-    // Stop waiting for the next cycle, if that is the current thread state.
-    cycleStatusCV.notify_all();
-
-    // Join the thread afterwards.
-    if (t.joinable()) {
-        t.join();
-    }
-}
-
-/*
-    Performs one cycle of the CPU.
-*/
-void CPU::cycle() {
-    // std::cout << "Start of cycle\n";
-    // Increment the requested CPU cycle count.
-    std::unique_lock lck(cycleStatusMutex);
-    cyclesRequested++;
-    cycleStatusCV.notify_all();
+    runOpcode(read());
 }
 
 void CPU::handleNonMaskableInterrupt() {
